9c33472347f517870b4e9c6a8205affef481a1cf
igotyb <igotyb.stream@gmail.com>
2023-02-24 12:07:57 +0100

    init of project - installation react - typescript - sass

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './style/style.scss'
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+    <h1>Hello World</h1>
+  </React.StrictMode>
+);
