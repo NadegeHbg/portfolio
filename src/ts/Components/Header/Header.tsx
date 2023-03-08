ff22454d0095ae47989f9cbcd685645d2bd04663
igotyb <igotyb.stream@gmail.com>
2023-03-08 13:29:35 +0100

    responsivness on navbar

--- src/ts/Components/Header/Header.tsx
+++ src/ts/Components/Header/Header.tsx
@@ -1,22 +1,41 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+
 import twitch from "../../../assets/logos/twitch.png"
 import linkedin from "../../../assets/logos/linkedin.png"
 
-
 const Header = () => {
+
+    const [toggle, setToggle] = useState(false);
+    const handleToggle = () => {
+        setToggle(!toggle)
+    }
+
     return (
-        <div className="nav">
-            <Link to="/" className="link margin-r" >Welcome !</Link>
-            <Link to="/portfolio" className="link margin-r" >Portfolio</Link>
-            <Link to="/resume" className="link margin-r" >Resume</Link>
-            <Link to="/contact" className="link margin-r" >Contact</Link>
-            <a href="https://www.linkedin.com/in/Nadege-Hbg" className="margin-r" target='_blank' rel="noreferrer">
-                <img src={linkedin} alt="linkedin logo" />
-            </a>
-            <a href="https://www.twitch.tv/igotyb" className="margin-r" target='_blank' rel="noreferrer">
-                <img src={twitch} alt="twitch logo" />
-            </a>
-        </div>
+        // <div className="nav">
+        //     <div id="desktop">
+
+        //     </div>
+
+        // </div>
+        <nav className={toggle ? 'nav expanded' : 'nav'}>
+            <div id="mobile" onClick={(handleToggle)}>
+                {toggle ? <i className="fa-solid fa-xmark icon"></i> : <i className="fa-solid fa-bars icon"></i>}
+            </div>
+            <div className="links">
+                <Link to="/" className="link" >Welcome !</Link>
+                <Link to="/portfolio" className="link" >Portfolio</Link>
+                <Link to="/resume" className="link" >Resume</Link>
+                <Link to="/contact" className="link" >Contact</Link>
+                <a href="https://www.linkedin.com/in/Nadege-Hbg" target='_blank' rel="noreferrer">
+                    <img src={linkedin} alt="linkedin logo" />
+                </a>
+                <a href="https://www.twitch.tv/igotyb" className="margin-r" target='_blank' rel="noreferrer">
+                    <img src={twitch} alt="twitch logo" />
+                </a>
+            </div>
+
+        </nav>
     )
 }
 
